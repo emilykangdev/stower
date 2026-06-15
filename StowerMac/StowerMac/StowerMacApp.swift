e048cf7146275f79d24f522a46488c03478cee9c
Emily-Kang77 <emilykang135@gmail.com>
2026-06-14 18:00:05 -0700

    Add StowerMac Xcode app scaffold

---
+++ StowerMac/StowerMac/StowerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  StowerMacApp.swift
+//  StowerMac
+//
+//  Created by Emily Kang on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct StowerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
